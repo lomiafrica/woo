c6b56a031b1e815bbd39d953a93ee31cf9ba55c3
PrinceMuichkine <babacar.diop.bba@edhec.com>
2026-05-08 08:22:09 +0000

    feat: initialize Woo plugin for Lomi

---
+++ assets/js/blocks/frontend/blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'b3f0465b39219d80a84a');
---
+++ assets/js/blocks/frontend/blocks/paystack-one.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '19b593b395bfb18dbe02');
